68b5e6db0f8aaeaa377c2b7ab5a39a007498c558
stevanjazziel-ui <stevanjazziel@gmail.com>
2026-07-08 15:01:47 -0500

    Actualizar asignaciones compartidas IPRUS (2026-07-08T20:01:47.111Z)

--- tramites-iprus-shared-state.js
+++ tramites-iprus-shared-state.js
@@ -1,4 +1,19 @@
 window.TRAMITES_IPRUS_SHARED_STATE = {
-  "generatedAt": "",
-  "assignments": {}
+  "title": "TRAMITES IPRUS SHARED ASSIGNMENTS",
+  "sourceDate": "2026-07-08T14:38:19",
+  "generatedAt": "2026-07-08T20:01:46.033Z",
+  "assignments": {
+    "GADMR--2026-0341-IPRUS-ZNP": "Miriam Guisha",
+    "GADMR--2026-0347-IPRUS-ZNP": "Hamilton Torres",
+    "GADMR--2026-0362-IPRUS-ZNP": "Fausto Lopez",
+    "GADMR--2026-0359-IPRUS-ZNP": "Miriam Guisha",
+    "GADMR--2026-0335-IPRUS-ZNP": "Hamilton Torres",
+    "GADMR--2026-0330-IPRUS-ZNP": "Fausto Lopez",
+    "GADMR--2026-0326-IPRUS-ZNP": "Miriam Guisha",
+    "GADMR--2026-0333-IPRUS-ZNP": "Hamilton Torres",
+    "GADMR--2026-0328-IPRUS-ZNP": "Fausto Lopez",
+    "GADMR--2026-0316-IPRUS-ZNP": "Miriam Guisha",
+    "GADMR--2026-0303-IPRUS-ZNP": "Hamilton Torres",
+    "GADMR--2026-0310-IPRUS-ZNP": "Fausto Lopez"
+  }
 };
